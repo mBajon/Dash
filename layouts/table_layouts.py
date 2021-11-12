3f523288d421fe22737f957febf388e00e86f766
mbajon <maciej.bajon@wp.pl>
2021-11-12 12:47:31 +0100

    code fromat changes + new test

--- layouts/table_layouts.py
+++ layouts/table_layouts.py
@@ -2,19 +2,12 @@ from dash import html
 import dash_bootstrap_components as dbc
 
 
-def generate_table(data : dict, headers: list)-> None:
-    return dbc.Table([
-        html.Thead(
-            html.Tr([
-                html.Th(headers[0], colSpan=2)
-                ])
-        ),
-        html.Tbody([
-                html.Tr([html.Td(i), html.Td(data[i])]) for i in data.keys()
-                ])],
-    className = 'table',
-    id={
-        'type': 'dynamic-table',
-        'index': headers[0]
-        }
+def generate_table(data: dict, headers: list) -> None:
+    return dbc.Table(
+        [
+            html.Thead(html.Tr([html.Th(headers[0], colSpan=2)])),
+            html.Tbody([html.Tr([html.Td(i), html.Td(data[i])]) for i in data.keys()]),
+        ],
+        className="table",
+        id={"type": "dynamic-table", "index": headers[0]},
     )
--- utils/constants.py
+++ utils/constants.py
@@ -1,38 +1,44 @@
 EXTERNAL_STYLESHEETS = []
-MARKS ={
-        1: {'label': '5y'},   
-        2: {'label': '3y'},
-        3: {'label': '1y'}, 
-        4: {'label': '6m'},
-        5: {'label': '3m'},
-        6: {'label': '1m'},
-        7: {'label': '7d'}
+MARKS = {
+    1: {"label": "5y"},
+    2: {"label": "3y"},
+    3: {"label": "1y"},
+    4: {"label": "6m"},
+    5: {"label": "3m"},
+    6: {"label": "1m"},
+    7: {"label": "7d"},
 }
 
-TIME_FRAMES= {
-        1:'5Y',
-        2:'3Y',
-        3:'6M',
-        4:'3M',
-        5:'1M',
-        6:'7D'
-    }
+TIME_FRAMES = {1: "5Y", 2: "3Y", 3: "6M", 4: "3M", 5: "1M", 6: "7D"}
 
 FIELD_MAP = {
-    'fiftyTwoWeekHigh':'52 Week High',
-    'fiftyTwoWeekLow':'52 Week Low',
-    'fiftyDayAverage':'50 Day Average',
-    'regularMarketVolume':'Regular Market Volume',
-    'averageDailyVolume10Day':'Average Daily Volumne in 10 days',
-    'volume24Hr':'Volume',
-    'longName':'Name',
-    'country':'Country',
-    'exchange':'Exchange',
-    'sector':'Sector',
-    'industry':'Industry',
-    'fullTimeEmployees':'Full Time Employees',
-    'marketCap':'Market Capitalization',
+    "fiftyTwoWeekHigh": "52 Week High",
+    "fiftyTwoWeekLow": "52 Week Low",
+    "fiftyDayAverage": "50 Day Average",
+    "regularMarketVolume": "Regular Market Volume",
+    "averageDailyVolume10Day": "Average Daily Volumne in 10 days",
+    "volume24Hr": "Volume",
+    "longName": "Name",
+    "country": "Country",
+    "exchange": "Exchange",
+    "sector": "Sector",
+    "industry": "Industry",
+    "fullTimeEmployees": "Full Time Employees",
+    "marketCap": "Market Capitalization",
 }
-TRADING_DATA=['fiftyTwoWeekHigh','fiftyTwoWeekLow','fiftyDayAverage', 'regularMarketVolume','averageDailyVolume10Day']
-GENERAL_INFO=['longName','country','exchange', 'sector','industry', 'fullTimeEmployees', 'marketCap']
-
+TRADING_DATA = [
+    "fiftyTwoWeekHigh",
+    "fiftyTwoWeekLow",
+    "fiftyDayAverage",
+    "regularMarketVolume",
+    "averageDailyVolume10Day",
+]
+GENERAL_INFO = [
+    "longName",
+    "country",
+    "exchange",
+    "sector",
+    "industry",
+    "fullTimeEmployees",
+    "marketCap",
+]
