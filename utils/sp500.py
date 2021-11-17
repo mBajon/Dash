daf7e5b7077b85ee3949668f9034b3f17bd002f8
mbajon <maciej.bajon@wp.pl>
2021-11-17 15:07:00 +0100

    changed readme

--- utils/sp500.py
+++ utils/sp500.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def get_tickers():
+def get_tickers()-> DataFrame:
     table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     df = table[0]
     return df[['Symbol','Security']].copy()
